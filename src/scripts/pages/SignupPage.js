e1ab0e78a9d47deccad2396bb601b58d42d9c658
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-03-29 17:17:54 -0700

    Add Christian's email list signup to the signup page

--- src/scripts/pages/SignupPage.js
+++ src/scripts/pages/SignupPage.js
@@ -14,10 +14,14 @@ var SignupPage = React.createClass({
   render: function () {
     return (
       <FullLayout id="SignupPage">
-        <h1>Sorry, signup is not available at this time</h1>
-        <p>
-          Please wait for an email letting you know when you can sign up
-        </p>
+        <h3>Register for an invite</h3>
+        <form action="http://christianbattista.us7.list-manage.com/subscribe/post?u=d140eca9cfe4a96473dac6ea5&amp;id=fba08af7dd" method="post" target="_blank">
+          <input type="email" value="" name="EMAIL" placeholder="Enter email address" size="30" required />
+          <div style={{"position": "absolute", "left": "-5000px"}}>
+            <input type="text" name="b_d140eca9cfe4a96473dac6ea5_fba08af7dd" value="" />
+          </div>
+          <button type="submit">Request invite</button>
+        </form>
       </FullLayout>
     );
   }
