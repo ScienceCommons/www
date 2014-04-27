74217fc546c96ad905293bf913b875bdf2a40c27
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-04-27 01:13:57 -0700

    Wire up a bit more of the login page

--- client/scripts/pages/LoginPage.js
+++ client/scripts/pages/LoginPage.js
@@ -11,17 +11,28 @@ var FullLayout = require("../layouts/FullLayout.js");
 require("../../styles/pages/LoginPage.scss");
 
 var LoginPage = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+  getInitialState: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
   componentDidMount: function() {
     this.refs.email.getDOMNode().focus();
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    console.log("login submit", this.state);
   },
   /*jshint ignore:start */
   render: function () {
     return (
       <FullLayout id="LoginPage">
-        <h3>Log in</h3>
-        <form>
-          <input type="text" size="30" placeholder="Email" ref="email"/>
-          <input type="password" size="30" placeholder="Password" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" size="30" placeholder="Email" ref="email" valueLink={this.linkState("email")} />
+          <input type="password" size="30" placeholder="Password" valueLink={this.linkState("password")} />
+          <input className="btn" type="submit" value="Log in" />
         </form>
       </FullLayout>
     );
