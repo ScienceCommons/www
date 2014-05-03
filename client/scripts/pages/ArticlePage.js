2d29099d5d12ca1949f650f2db9b639617528b4e
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-05-03 15:50:37 -0700

    Article page: Get a basic version of the replications table working
    Add some more background icons

--- client/scripts/pages/ArticlePage.js
+++ client/scripts/pages/ArticlePage.js
@@ -69,8 +69,122 @@ var ArticlePage = React.createClass({
             </tbody>
           </table>
 
-          <div className="Replications">
+          <div>
             <h1>Replications</h1>
+            <table className="Replications">
+              <thead>
+                <tr>
+                  <th>Replication path</th>
+                  <th colSpan="3" className="ReplicationPath">
+                    <div className="study"></div>
+                    <div className="replication open"></div>
+                    <div className="study"></div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Authors &amp; Study</td>
+                  <td>Zhong et al.</td>
+                  <td className="replication">Zhong et al.</td>
+                  <td>Zhong et al.</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td className="badges">
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_data"></i>
+                    </i>
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_methods"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_registration"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_disclosure"></i>
+                    </i>
+                  </td>
+                  <td className="badges replication">
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_data"></i>
+                    </i>
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_methods"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_registration"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_disclosure"></i>
+                    </i>
+                  </td>
+                  <td className="badges">
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_data"></i>
+                    </i>
+                    <i className="badge blue icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_methods"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_registration"></i>
+                    </i>
+                    <i className="badge icon icon_bkgr_squircle">
+                      <i className="icon icon_sml_disclosure"></i>
+                    </i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Independent variables</td>
+                  <td>
+                    Transcribe unethical vs ethical deed
+                  </td>
+                  <td className="replication">
+                    Transcribe unethical vs ethical deed
+                  </td>
+                  <td>
+                    Transcribe unethical vs ethical deed
+                  </td>
+                </tr>
+                <tr>
+                  <td>Dependent variables</td>
+                  <td>
+                    Desirability of cleaning-related products
+                  </td>
+                  <td className="replication">
+                    Desirability of cleaning-related products
+                  </td>
+                  <td>
+                    Desirability of cleaning-related products
+                  </td>
+                </tr>
+                <tr>
+                  <td>N <span className="icon icon_person"></span></td>
+                  <td>27</td>
+                  <td className="replication">27</td>
+                  <td>27</td>
+                </tr>
+                <tr>
+                  <td>Power</td>
+                  <td>86%</td>
+                  <td className="replication">86%</td>
+                  <td>27</td>
+                </tr>
+                <tr>
+                  <td>Effect size</td>
+                  <td>d=1.08</td>
+                  <td className="replication">d=1.08</td>
+                  <td>d=1.08</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="4">
+                    <button className="btn">View Graph</button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
           </div>
 
           <table>
@@ -84,12 +198,12 @@ var ArticlePage = React.createClass({
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, venenatis molestie ligula in, lobortis lobortis est. Nunc adipiscing erat sed libero volutpat dapibus ultrices feugiat elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent ac nisi luctus arcu tempus malesuada. Fusce lectus augue, ultrices id purus ac, viverra convallis ipsum. Mauris convallis urna ut magna laoreet, quis dapibus dolor aliquet. Nunc tristique pulvinar imperdiet. Fusce et lectus ac nunc porta eleifend imperdiet sed diam. Curabitur sollicitudin id enim a lacinia. Suspendisse ultricies laoreet turpis a tempor. Cras dapibus, dolor quis ultrices convallis, sapien lectus blandit turpis, in mollis purus elit ac magna.
                     </p>
                   </div>
+                  <div>
                     <h3>Comments</h3>
                     <form>
                       <input type="text" placeholder="Add a comment" />
                       <button type="submit" className="btn">Post</button>
                     </form>
-                  <div>
                   </div>
                 </td>
                 <td>
