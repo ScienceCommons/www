f6033c6f024ab78bad246e45e12a3b3cc6e77b3e
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-05-10 22:35:07 -0700

    Get the profile page working again

--- client/pages/ProfilePage.js
+++ client/pages/ProfilePage.js
@@ -4,46 +4,49 @@
 
 var Layout = require("../layouts/DefaultLayout.js");
 
+var _ = require("underscore");
+
 var ProfilePage = {};
 
 ProfilePage.controller = function(options) {
   options = _.extend({id: "ProfilePage"}, options);
   this.layoutController = new Layout.controller(options);
+  this.user = options.user;
 };
 
 ProfilePage.view = function(ctrl) {
-  var user = ctrl.user.cortex;
+  var user = ctrl.user;
   var articlesContent;
-  if (user.articles.count() === 0) {
+  if (user.get("articles").length === 0) {
     articlesContent = (
       <p>
         You have no articles. You can link articles by searching for them and then requesting to be linked.
       </p>
     );
   } else {
-    var list = user.articles.map(function(article) {
-      return <li>{article.title.val()}</li>
+    var list = _.map(user.get("articles"), function(article) {
+      return <li>{article.title}</li>
     });
     articlesContent = <ul>{list}</ul>;
   }
 
   var commentsContent;
-  if (user.comments.count() === 0) {
+  if (user.get("comments").length === 0) {
     commentsContent = (
       <p>
         You have no comments.
       </p>
     );
   } else {
-    var list = user.comments.map(function(comment) {
-      return <li>{comment.body.val()}</li>
+    var list = _.map(user.get("comments"), function(comment) {
+      return <li>{comment.body}</li>
     });
     commentsContent = <ul>{list}</ul>;
   }
 
   var content = (
     <div>
-      <h1 className="h1">{[user.first_name.val(), user.middle_name.val(), user.last_name.val()].join(" ")} ({user.email.val()})</h1>
+      <h1 className="h1">{[user.get("first_name"), user.get("middle_name"), user.get("last_name")].join(" ")} ({user.get("email")})</h1>
 
       <h3>Articles</h3>
       {articlesContent}
