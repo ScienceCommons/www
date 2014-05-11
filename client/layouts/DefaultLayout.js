f4ef2c4350942a4a7d8a4c6aab8ba6357ceafe3d
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-05-10 17:14:37 -0700

    Move page header into default layout

--- client/layouts/DefaultLayout.js
+++ client/layouts/DefaultLayout.js
@@ -3,20 +3,36 @@
 "use strict";
 require("./DefaultLayout.scss");
 
-var PageHeader = require("../components/PageHeader.js");
+var m = require("mithril");
+
+var Search = require("../components/Search.js");
+var UserBar = require("../components/UserBar.js");
+var Logo = require("../components/Logo.js");
 
 var DefaultLayout = {};
 
 DefaultLayout.controller = function(options) {
   options = options || {};
   this.id = options.id
-  this.pageHeaderController = new PageHeader.controller({user: options.user, query: options.query})
+  this.userBarController = new UserBar.controller({user: options.user})
+  this.searchController = new Search.controller({query: m.route.param("query")});
 };
 
 DefaultLayout.view = function(ctrl, content) {
+  if (ctrl.user) {
+    var userBar = new UserBar.view(ctrl.userBarController);
+  }
+
   return (
     <div id={ctrl.id} className="page DefaultLayout">
-      {new PageHeader.view(ctrl.pageHeaderController)}
+      <header>
+        <div className="banner">
+          {userBar}
+          <a href="/" config={m.route} className="logoLink">{new Logo.view()}</a>
+        </div>
+        {new Search.view(ctrl.searchController)}
+      </header>
+
       <div className="content">{content}</div>
     </div>
   );
