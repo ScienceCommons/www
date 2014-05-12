0180cf5157362a02c697b1428c1d1af8df33c6c7
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-05-11 22:02:33 -0700

    Turn off uglify for now since it is blowing away style
    Add occurrence order optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,8 +121,8 @@ module.exports = function (grunt) {
           "mithril": "m"
         },
         plugins: [
-          new webpack.optimize.DedupePlugin(),
-          new webpack.optimize.UglifyJsPlugin()
+          new webpack.optimize.OccurenceOrderPlugin(),
+          new webpack.optimize.DedupePlugin()
         ]
       }
     },
