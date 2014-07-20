cccb48a56997cbc6274fe752502121155e9204ce
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-20 13:32:12 -0700

    ArticleCollection: remove unnecessary mithril require

--- client/collections/ArticleCollection.js
+++ client/collections/ArticleCollection.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var _ = require("underscore");
-var m = require("mithril");
 var CurateBaseCollection = require("./CurateBaseCollection.js");
 
 var ArticleCollection = CurateBaseCollection.extend({
