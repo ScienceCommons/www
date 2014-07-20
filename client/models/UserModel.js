99e0f9b88dcc5030cb0558439e9e50f6be234bee
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-20 12:16:20 -0700

    UserModel: clean up defaults

--- client/models/UserModel.js
+++ client/models/UserModel.js
@@ -15,16 +15,13 @@ var UserModel = CurateBaseModel.extend({
   },
   defaults: {
     "email": "stephen@curatescience.org",
+    "name": "Stephen Demjanenko",
     "first_name": "Stephen",
     "middle_name": "",
     "last_name": "Demjanenko",
     "facebook": "sdemjanenko",
     "twitter": "sdemjanenko",
     "gravatar": "8c51e26145bc08bb6f43bead1b5ad07f.png", // me
-    "notifications": [
-      {title: "foo", body: "foo body", read: false},
-      {title: "bar", body: "bar body", read: true}
-    ],
     "areas_of_study": ["Astrophysics", "Cosmology"],
     "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis nulla in turpis luctus rutrum. Quisque adipiscing leo fringilla enim luctus ultricies. Fusce iaculis augue tincidunt eleifend condimentum. Vestibulum commodo massa ut vulputate aliquam. Etiam eu ante id est varius auctor. Sed fermentum at purus ac pellentesque. Duis nibh est, ornare ac tellus a, fermentum porta velit. In in risus et orci rhoncus egestas.\n\nNulla facilisi. Proin iaculis, nisl dictum consequat tincidunt, lectus arcu tincidunt magna, a placerat purus dui vitae dui. Maecenas fermentum luctus sodales. Cras vestibulum, erat in gravida tristique, augue ante scelerisque diam, non porta sem metus.",
     "bookmarks": [12552, 35395] // article ids
