80860f5adf182a47cb17480929d4d6e4fc21fc93
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-20 13:33:02 -0700

    AdminPage: add user search

--- client/pages/AdminPage.js
+++ client/pages/AdminPage.js
@@ -4,8 +4,11 @@
 require("./AdminPage.scss");
 
 var _ = require("underscore");
+var m = require("mithril");
+
 var Layout = require("../layouts/DefaultLayout.js");
 var OnUnload = require("../utils/OnUnload.js");
+var UserCollection = require("../collections/UserCollection.js");
 
 var AdminPage = {};
 
@@ -13,12 +16,41 @@ AdminPage.controller = function(options) {
   OnUnload(this);
   options = _.extend({id: "AdminPage"}, options);
   this.controllers.layout = new Layout.controller(options);
+
+  this.query = m.prop("");
+  this.users = new UserCollection([]);
+
+  var _this = this;
+  this.handleSubmit = function(e) {
+    e.preventDefault();
+    _this.users.search({query: _this.query()});
+  };
 };
 
 AdminPage.view = function(ctrl) {
+  var results;
+  if (ctrl.query()) {
+    if (ctrl.users.loading) {
+      results = "Searching...";
+    } else if (ctrl.users.error) {
+      results = "Error: " + ctrl.users.error;
+    } else if(ctrl.users.length > 0) {
+      var list = ctrl.users.map(function(user) {
+        return <li>{user.get("name")}</li>;
+      });
+      results = <ul>{list}</ul>;
+    }
+  }
+
   var content = (
     <div>
-      Admin page
+      <h1>Administration</h1>
+
+      <h3>Find users</h3>
+      <form onsubmit={ctrl.handleSubmit}>
+        <input type="text" value={ctrl.query()} oninput={m.withAttr("value", ctrl.query)} />
+      </form>
+      {results}
     </div>
   );
 
