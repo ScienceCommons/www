7562228fbd26b9e116f71708da288544d54f6195
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-08-02 11:57:49 -0700

    LoginPage: post email/password to the server

--- client/pages/LoginPage.js
+++ client/pages/LoginPage.js
@@ -4,6 +4,7 @@
 require("./LoginPage.scss");
 
 var _ = require("underscore");
+var m = require("mithril");
 
 var OnUnload = require("../utils/OnUnload.js");
 var Layout = require("../layouts/FullLayout.js");
@@ -16,18 +17,32 @@ LoginPage.controller = function(options) {
 
   this.email = m.prop("");
   this.password = m.prop("");
+  this.error = m.prop(false);
 
   var _this = this;
   this.login = function(e) {
     e.preventDefault();
-    if (_this.email() === "stephen@curatescience.org" && _this.password() === "rabbitears") {
+
+    _this.error(false);
+    m.request({method: "POST", url: "https://www.curatescience.org/users/sign_in.json", data: {
+      password: _this.password(),
+      email: _this.email()
+    }}).then(function(data) {
+      console.log("logged in", data);
       CS.user = {};
       m.route("/");
-    }
+    }, function(err) {
+      console.log("failed to log in", err);
+      _this.error("Log in failed: Wrong email and/or password");
+    });
   };
 };
 
 LoginPage.view = function(ctrl) {
+  if (ctrl.error()) {
+    var error = <h3>{ctrl.error()}</h3>;
+  }
+
   var content = (
     <div>
       <form onsubmit={ctrl.login}>
@@ -35,6 +50,7 @@ LoginPage.view = function(ctrl) {
         <input type="password" size="30" placeholder="Password" value={ctrl.password()} oninput={m.withAttr("value", ctrl.password)} />
         <input className="btn" type="submit" value="Log in" />
       </form>
+      {error}
     </div>
   );
 
