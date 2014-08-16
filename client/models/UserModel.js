4694926fc47a15acf5f5674e68bc4d1172257fe4
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-08-16 14:32:41 -0400

    UserModel: update bookmarks since the DB got cleared

--- client/models/UserModel.js
+++ client/models/UserModel.js
@@ -26,7 +26,7 @@ var UserModel = CurateBaseModel.extend({
     "gravatar": "8c51e26145bc08bb6f43bead1b5ad07f.png", // me
     "areas_of_study": ["Astrophysics", "Cosmology"],
     "about": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis nulla in turpis luctus rutrum. Quisque adipiscing leo fringilla enim luctus ultricies. Fusce iaculis augue tincidunt eleifend condimentum. Vestibulum commodo massa ut vulputate aliquam. Etiam eu ante id est varius auctor. Sed fermentum at purus ac pellentesque. Duis nibh est, ornare ac tellus a, fermentum porta velit. In in risus et orci rhoncus egestas.\n\nNulla facilisi. Proin iaculis, nisl dictum consequat tincidunt, lectus arcu tincidunt magna, a placerat purus dui vitae dui. Maecenas fermentum luctus sodales. Cras vestibulum, erat in gravida tristique, augue ante scelerisque diam, non porta sem metus.",
-    "bookmarks": [12552, 35395] // article ids
+    "bookmarks": [52529, 51037] // article ids
   },
   computeds: {
     facebookUrl: function() {
