c90f8d187fd8b0261dbac3346b8e26df017d2bb0
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-08-31 11:32:50 -0700

    Authors: stub out the model and start working on a useful author page

--- client/models/AuthorModel.js
+++ client/models/AuthorModel.js
@@ -7,10 +7,33 @@ var _ = require("underscore");
 var CurateBaseModel = require("./CurateBaseModel.js");
 
 var AuthorModel = CurateBaseModel.extend({
+  //relations: {
+  //  "articles": {type: "many", model: require("./ArticleModel.js"), urlAction: "articles"}
+  //},
   defaults: {
-    first_name: "",
-    last_name: "",
-    middle_name: ""
+    first_name: "John",
+    last_name: "Smith",
+    middle_name: "Rob",
+    orcid: "",
+    affiliation: "",
+    email: "john.rob.smith@gmail.com",
+    is_user: "",
+    articles: [
+      {
+        title: "Feeling the future: Experimental evidence for anomalous retroactive influences on congnition and affect",
+        authors_denormalized: [{lastName: "Bern"}],
+        publication_date: "2011-6-1"
+      }, {
+        title: "Automaticity of social behavior: Direct effects of trait construct and stereotype activiation on action",
+        authors_denormalized: [{lastName: "Bargh"}, {lastName: "Chen"}, {lastName: "Burrows"}],
+        publication_date: "1996-6-1"
+      },
+      {
+        title: "Coherent arbitrariness: Stable demand curves without stable preference",
+        authors_denormalized: [{lastName: "Airely"}],
+        publication_date: "2003-6-1"
+      }
+    ]
   },
   computeds: {
     fullName: function() {
--- client/pages/AuthorPage.js
+++ client/pages/AuthorPage.js
@@ -8,7 +8,7 @@ var m = require("mithril");
 var OnUnload = require("../utils/OnUnload.js");
 var Layout = require("../layouts/DefaultLayout.js");
 var Spinner = require("../components/Spinner.js");
-var UserModel = require("../models/UserModel.js");
+var AuthorModel = require("../models/AuthorModel.js");
 
 var AuthorPage = {};
 
@@ -17,27 +17,38 @@ AuthorPage.controller = function(options) {
   options = _.extend({id: "AuthorPage"}, options);
   this.controllers.layout = new Layout.controller(options);
   this.loading = m.prop(true);
-  this.author = new UserModel({id: options.authorId}, {callback: m.redraw, loading: true})
+  this.author = new AuthorModel();
+  window.author = this.author;
 };
 
 AuthorPage.view = function(ctrl) {
-  var loading = ctrl.author.loading;
-  var author = ctrl.author.cortex;
-  var content;
-
-  if (loading) {
-    content = new Spinner.view();
-  } else if (author) {
-    content = (
+  var author = ctrl.author;
+  var articles = author.get("articles");
+  if (articles.length > 0) {
+    var articlesList = _.map(articles, AuthorPage.articleView);
+    var articlesContent = (
       <div>
-        <h1 className="h1">{author.name.val()}</h1>
+        <h3>{articles.length} Article{articles.length > 1 ? "s" : ""}</h3>
+        <ul>
+          {articlesList}
+        </ul>
       </div>
     );
-  } else {
-    content = <h1>Author not found</h1>
   }
 
+  var content = (
+    <div>
+      <h1 className="h1">{author.get("fullName")}</h1>
+      <h5>{author.get("email")}</h5>
+      {articlesContent}
+    </div>
+  );
+
   return new Layout.view(ctrl.controllers.layout, content);
+};
+
+AuthorPage.articleView = function(article) {
+  return <h5>{article.title}</h5>;
 };
 
 module.exports = AuthorPage;
