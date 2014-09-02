1fa5561ccaa6a8562b5b074125ae3e71070f6b10
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-09-01 20:44:24 -0700

    BaseData: fix minor style nit

--- client/models/BaseData.js
+++ client/models/BaseData.js
@@ -593,7 +593,7 @@ var extend = function(protoProps, staticProps) {
   }
   if (_.isArray(protoProps.relations)) {
     if (_.all(_.first(protoProps.relations, protoProps.relations.length-1), function(dependency) { return loaded[dependency]; })) {
-      _.extend(child.prototype, {relations: _.last(protoProps.relations)()});
+      child.prototype.relations = _.last(protoProps.relations)();
     } else {
       waitingForDependency.push(protoProps.relations.concat([child]));
     }
@@ -601,7 +601,7 @@ var extend = function(protoProps, staticProps) {
   var finished = [];
   _.each(waitingForDependency, function(waitingModel) {
     if (_.all(_.first(waitingModel, waitingModel.length-2), function(dependency) { return loaded[dependency]; })) {
-      _.extend(_.last(waitingModel).prototype, {relations: waitingModel[waitingModel.length-2]()});
+      _.last(waitingModel).prototype.relations = waitingModel[waitingModel.length-2]();
       finished.push(waitingModel);
     }
   });
