42c786d9252494f04ff550b874ffc93fd35bfa41
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-11-20 22:43:47 -0800

    BookmarksPage: render toggle button, year and authors

--- client/pages/BookmarksPage.js
+++ client/pages/BookmarksPage.js
@@ -27,7 +27,7 @@ BookmarksPage.view = function(ctrl) {
 
   if (bookmarks.length > 0) {
     var list = bookmarks.map(function(bookmark) {
-      return BookmarksPage.views[bookmark.get("bookmarkable_type")](bookmark.get("bookmarkable"));
+      return BookmarksPage.views[bookmark.get("bookmarkable_type")](bookmark.get("bookmarkable"), ctrl.user);
       // return <li><a href={"/articles/"+bookmark.get("bookmarkable_id")} config={m.route}>{bookmark.get("bookmarkable_id")}</a></li>;
     });
 
@@ -53,9 +53,15 @@ BookmarksPage.view = function(ctrl) {
 
 BookmarksPage.views = {};
 
-BookmarksPage.views.Article = function(article) {
+BookmarksPage.views.Article = function(article, user) {
   return <li className="bookmark articleBookmark">
-    <a href={article.url().replace(window.location.origin, "")} config={m.route}>{article.get("title")}</a>
+    <header><a href={article.url().replace(window.location.origin, "")} config={m.route}>{article.get("title")}</a></header>
+    <div className="authors">
+      <button type="button" className={"btn btn_subtle bookmark " + (user.hasBookmarked("Article", article.get("id")) ? "active" : "")} onclick={user.toggleBookmark("Article", article)}>
+        <span className="icon icon_bookmark"></span>
+      </button>
+      ({article.get("year")}) {article.authors().etAl(3)}
+    </div>
   </li>;
 };
 
