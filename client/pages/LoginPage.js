21796d5c5d693579d6d0ee3f8f91583971b2d367
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-12-01 08:11:34 -0800

    LoginPage: add signup form

--- client/pages/LoginPage.js
+++ client/pages/LoginPage.js
@@ -19,12 +19,25 @@ LoginPage.controller = function(options) {
 LoginPage.view = function(ctrl) {
   var content = (
     <div>
-      <a href="/auth/google_oauth2">
-        <div className="Y3d isa Ndb" title="Sign in with Google" role="button" tabindex="0">
-          <div className="Ega"></div>
-          <div className="tLb">Sign in</div>
-        </div>
-      </a>
+      <div>
+        <a href="/auth/google_oauth2">
+          <div className="Y3d isa Ndb" title="Sign in with Google" role="button" tabindex="0">
+            <div className="Ega"></div>
+            <div className="tLb">Sign in</div>
+          </div>
+        </a>
+      </div>
+      <h3>or</h3>
+      <div>
+        <h3>Register for an invite</h3>
+        <form action="http://christianbattista.us7.list-manage.com/subscribe/post?u=d140eca9cfe4a96473dac6ea5&amp;id=fba08af7dd" method="post" target="_blank">
+          <input type="email" value="" ref="email" name="EMAIL" placeholder="Enter email address" size="30" required />
+          <div style={{"position": "absolute", "left": "-5000px"}}>
+            <input type="text" name="b_d140eca9cfe4a96473dac6ea5_fba08af7dd" value="" />
+          </div>
+          <button type="submit" className="btn">Request invite</button>
+        </form>
+      </div>
     </div>
   );
 
