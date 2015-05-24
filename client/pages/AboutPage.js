852d5d67b14b5d5857c1ac787b5606e12c73e098
Etienne P. LeBel <etiennelebel@yahoo.ca>
2015-05-24 09:25:08 -0400

    Update AboutPage.js

--- client/pages/AboutPage.js
+++ client/pages/AboutPage.js
@@ -20,24 +20,13 @@ AboutPage.view = function(ctrl) {
     <div>
       <h1>About</h1>
       <p>
-        Replicability is a cornerstone of science, but evidence of independent replications
-        is currently disorganized, making it difficult to gauge the reliability of empirical
-        findings. Independent verification of data and analyses is another hallmark of science,
-        yet is currently challenging in practice because data/syntax are difficult to track down.
+        Independent verification is the cornerstone of science, however, in practice, the independent verification of published findings is difficult and not rewarded. Hence, gauging the reliability and validity of published scientific findings is typically very difficult (if not impossible). <b>Curate Science</b> is a web application that aims to help in these regards by facilitating and incentivizing the independent verification of published scientific findings in several respects.
       </p>
       <p>
-        Curate Science aims to solve both of these problems by providing an online platform that
-        substantially facilitates these two crucial processes of science. Independent replication
-        information and available data/syntax files will automatically be aggregated from pre-existing
-        sources (e.g., from PsychFileDrawer.org, Figshare.com). Registered users will be able to add,
-        modify, and update such information, including in-line commenting directed at specific elements
-        of an article and in-browser analyses for articles with available R syntax files.
+        Independent verification is facilitated by enabling researchers to (1) link independent replications to original studies (and eventually linking conceptual replication studies meta-analytically), (2) curate available data/syntax to verify reproducibility of results within the browser, and (3) critically evaluate the interpretation/validity of findings via in-line comments (i.e., post-publication peer review on stereoids given evaluation done in the context of available data and replicability information).
       </p>
       <p>
-        Our vision is that building such a platform will accelerate the growth of cumulative knowledge
-        and innovation by facilitating and incentivizing independent replications and verification,
-        but also by facilitating the post-publication critique, evaluation, extension, re-analysis,
-        and interpretation of findings (see Features, for a flowchart of our core features).
+        Curate Science will incentivize independent verififation by (1) prominently displaying recent user contributions on the homepage to reward researchers and (2) offering unique high-utility features that motivate users to regularly use and engage with site content (e.g., notifications of new replications, new activity on articles a user has contributed to, and discovery of new related articles). Furthermore, knowing that other researchers can benefit from one's independent verification activities acts as an incentive to actually engage in those activities. For example, a researcher is more likely to verify the reproducibility of a study's results if they know other researchers' may benefit from those activities (e.g., researchers wanting to re-analyze a study's data from a different theoretical perspective can greatly benefit from a researcher who curated a study's data files to verify reproducibility). See <a href="#section-3">Features</a>, for a flowchart of our core features.
       </p>
     </div>
   );
