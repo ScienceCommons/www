53635f809b3e768e08f9e858ce09f98036ab1fda
Alex Kyllo <alex.kyllo@gmail.com>
2015-06-21 00:02:01 -0700

    updates to login page

--- client/pages/LoginPage.js
+++ client/pages/LoginPage.js
@@ -20,10 +20,11 @@ LoginPage.view = function(ctrl) {
   var content = (
     <div>
       <div>
+        <h3>Please use one of your social networking accounts to log into Curate Science:</h3>
         <a href="/auth/google_oauth2">
-          <div className="Y3d isa Ndb" title="Sign in with Google" role="button" tabindex="0">
+          <div className="Y3d isa Ndb" title="Login with Google+" role="button" tabindex="0">
             <div className="Ega"></div>
-            <div className="tLb">Sign in</div>
+            <div className="tLb">Login with Google+</div>
           </div>
         </a>
       </div>
