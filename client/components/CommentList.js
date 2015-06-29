095ca9681f9b84a954e3273091e7be8d8343a67b
Alex Kyllo <alex.kyllo@gmail.com>
2015-06-28 20:19:45 -0700

    show CommentList to anon users

--- client/components/CommentList.js
+++ client/components/CommentList.js
@@ -15,7 +15,7 @@ Comment.view = function(ctrl) {
   if (!comment.get("comments").isEmpty()) {
     var replies = new CommentList.view(ctrl.listController, {comments: comment.get("comments")});
   }
-  if (ctrl.onDelete && (comment.get("owner_id") === ctrl.listController.user.get("id") || ctrl.listController.user.get("admin"))) {
+  if (ctrl.user && ctrl.onDelete && (comment.get("owner_id") === ctrl.listController.user.get("id") || ctrl.listController.user.get("admin"))) {
     var deleteButton = <button type="button" className="btn" onclick={ctrl.onDelete}>Delete</button>;
   }
   if (ctrl.listController.reply) {
